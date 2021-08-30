f990ca70692cb77e097d9f453e355bc72e23565c
François Barrailla <francois.barrailla@ekino.com>
2021-08-30 16:59:57 +0700

    init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# HealthCare Locator SDK for Angular
+
+This project is an example how to integrate the <hcl-sdk-component> in an Angular project
+
+## To start project from root:
+
+Run `start-example-angular`. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="wrapper">
+      <hcl-sdk-component [config]="config"></hcl-sdk-component>
+    </div>
+  `,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  public config: any;
+  ngOnInit(): void {
+    // Set SDK configuration here
+    // @see https://docs.healthcarelocator.com/index.htm#t=QStartGuide%2FCustomize_the_prebuilt_screens_of_the_SDKs.htm
+    this.config = {
+      apiKey: 'INSERT_YOUR_API_KEY_HERE',
+      showSuggestModification: true,
+      enableMedicalTerm: true,
+    };
+  }
+}
